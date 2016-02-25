f4c800b9b53a0dbd181b70ceb4887e8a0b46eea0
josh.young11@gmail.com <f14tomc@t>
2016-02-24 22:28:23 -0500

    New Activity Added 2

---
+++ app/src/main/java/com/example/joshy_000/javaquiz/CongratActivity.java
@@ -0,0 +1,13 @@
+package com.example.joshy_000.javaquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CongratActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_congrat);
+    }
+}
